9acdcaaaf75aa9891bec44207b9d064d92a9e99a
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-05-11 08:26:53 +0300

    Add simple CLI

---
+++ cli.js
@@ -0,0 +1,12 @@
+const { execSync } = require("child_process");
+const main = () => {
+  const args = process.argv.slice(2);
+  switch (args[0]) {
+    case "start":
+      execSync("npm run start", { stdio: "inherit", cwd: "./" });
+      break;
+    default:
+      console.log("CLI simple as...");
+  }
+};
+main();
