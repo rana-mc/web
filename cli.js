2ed00721bfbde7dc8b81af1a41be203c7193cd1d
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-05-11 08:40:20 +0300

    Update CLI

--- cli.js
+++ cli.js
@@ -2,9 +2,11 @@
 const { execSync } = require("child_process");
 const main = () => {
   const args = process.argv.slice(2);
+  // TODO: Find better way?
+  const cwd = process.argv[1].replace("cli.js","");
   switch (args[0]) {
     case "start":
-      execSync("npm run start", { stdio: "inherit", cwd: "./" });
+      execSync("npm run start", { stdio: "inherit", cwd });
       break;
     default:
       console.log("CLI simple as...");
