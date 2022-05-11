46b46a45cd5a92b8aad095abcfbb5e6d6c2ef184
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-05-11 09:06:28 +0300

    Update CLI

--- cli.js
+++ cli.js
@@ -1,12 +1,22 @@
 #! /usr/bin/env node
 const { execSync } = require("child_process");
+const exec = require("child_process").exec;
+function getNPMRoot(callback) {
+  exec("npm root -g", function (error, stdout, stderr) {
+    callback(stdout);
+  });
+}
+const path = require("path");
 const main = () => {
   const args = process.argv.slice(2);
-  // TODO: Find better way?
-  const cwd = process.argv[1].replace("cli.js","");
   switch (args[0]) {
     case "start":
-      execSync("npm run start", { stdio: "inherit", cwd });
+      getNPMRoot(function (root) {
+        // TODO: Find better way?
+        const cwd = path.resolve(root.trim(), "@rana-mc/web");
+        console.log(`cwd: ${cwd}`);
+        execSync("npm run start", { stdio: "inherit", cwd });
+      });
       break;
     default:
       console.log("CLI simple as...");
